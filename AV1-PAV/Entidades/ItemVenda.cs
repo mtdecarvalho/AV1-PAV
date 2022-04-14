af1ad6c9991661b8e5e791ff6663103236603c9b
senhorbento <sbento.ti@gmail.com>
2022-04-14 13:02:29 -0300

    Renomeado Persistência -> Persistencia e modificado ordem de algums using

--- AV1-PAV/Entidades/ItemVenda.cs
+++ AV1-PAV/Entidades/ItemVenda.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MySql.Data.MySqlClient;
+
 using AV1_PAV.Persistencia;
+using MySql.Data.MySqlClient;
 
 namespace AV1_PAV.Entidades
 {
--- AV1-PAV/Persistencia/BancoDados.cs
+++ AV1-PAV/Persistencia/BancoDados.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 using MySql.Data.MySqlClient;
 
 namespace AV1_PAV.Persistencia
