4a821ffba49cf57d49b5f7a749fceeabc7781e1f
senhorbento <sbento.ti@gmail.com>
2022-04-24 12:12:41 -0300

    Desfeita alteracao nos scripts de insercao

--- AV1-PAV/BD/Insert_Clientes.sql
+++ AV1-PAV/BD/Insert_Clientes.sql
@@ -1,19 +1,19 @@
 use pav_av1;
 -- Tabela clientes
-INSERT INTO cliente(nome, cpf_cnpj, logradouro, numero, complemento, bairro, cidade, estado, cep, telefone, email)
-VALUES ("Cliente não identificado", 00000000000, "", 0,"","","","","","","");
+INSERT INTO cliente(id_cliente, nome, cpf_cnpj, logradouro, numero, complemento, bairro, cidade, estado, cep, telefone, email)
+VALUES (0,"Cliente não identificado", 00000000000, "", 0,"","","","","","","");
 
-INSERT INTO cliente(nome, cpf_cnpj, logradouro, numero, complemento, bairro, cidade, estado, cep, telefone, email)
-VALUES ("Vinícius Correia Fernandes", 25307158580, "Rua Edmundo Brito Mugnaine", 1186,"","Jardim Arruda","Limeira","SP","13482350","1974043066","ViniciusCorreiaFernandes@dayrep.com");
+INSERT INTO cliente(id_cliente, nome, cpf_cnpj, logradouro, numero, complemento, bairro, cidade, estado, cep, telefone, email)
+VALUES (1,"Vinícius Correia Fernandes", 25307158580, "Rua Edmundo Brito Mugnaine", 1186,"","Jardim Arruda","Limeira","SP","13482350","1974043066","ViniciusCorreiaFernandes@dayrep.com");
 
-INSERT INTO cliente(nome, cpf_cnpj, logradouro, numero, complemento, bairro, cidade, estado, cep, telefone, email)
-VALUES ("Ryan Fernandes Almeida", 76854586593, "Rua Campo Verde", 1770,"","Praia do Futuro II","Fortaleza","CE","60182502","8525224904","RyanFernandesAlmeida@teleworm.us");
+INSERT INTO cliente(id_cliente, nome, cpf_cnpj, logradouro, numero, complemento, bairro, cidade, estado, cep, telefone, email)
+VALUES (2,"Ryan Fernandes Almeida", 76854586593, "Rua Campo Verde", 1770,"","Praia do Futuro II","Fortaleza","CE","60182502","8525224904","RyanFernandesAlmeida@teleworm.us");
 
-INSERT INTO cliente(nome, cpf_cnpj, logradouro, numero, complemento, bairro, cidade, estado, cep, telefone, email)
-VALUES ("Grace Hogan", 28274454766, "Rua Braúlio", 1711,"","Santo Elias","Mesquita","RJ","26560581","2185829925","GraceHogan@dayrep.com");
+INSERT INTO cliente(id_cliente, nome, cpf_cnpj, logradouro, numero, complemento, bairro, cidade, estado, cep, telefone, email)
+VALUES (3,"Grace Hogan", 28274454766, "Rua Braúlio", 1711,"","Santo Elias","Mesquita","RJ","26560581","2185829925","GraceHogan@dayrep.com");
 
-INSERT INTO cliente(nome, cpf_cnpj, logradouro, numero, complemento, bairro, cidade, estado, cep, telefone, email)
-VALUES ("Fen Tsao", 74736575697, "Rua Peroba", 1234,"","Matinha","Abreu e Lima","PE","53550390","8162884940","FenTsao@jourrapide.com");
+INSERT INTO cliente(id_cliente, nome, cpf_cnpj, logradouro, numero, complemento, bairro, cidade, estado, cep, telefone, email)
+VALUES (4,"Fen Tsao", 74736575697, "Rua Peroba", 1234,"","Matinha","Abreu e Lima","PE","53550390","8162884940","FenTsao@jourrapide.com");
 
-INSERT INTO cliente(nome, cpf_cnpj, logradouro, numero, complemento, bairro, cidade, estado, cep, telefone, email)
-VALUES ("Melissa Marino", 81253272824, "Rua Mato Grosso", 1159,"","Bela Vista","Nova Friburgo","RJ","28623030","2241667853","MelissaMarino@teleworm.us");
+INSERT INTO cliente(id_cliente, nome, cpf_cnpj, logradouro, numero, complemento, bairro, cidade, estado, cep, telefone, email)
+VALUES (5,"Melissa Marino", 81253272824, "Rua Mato Grosso", 1159,"","Bela Vista","Nova Friburgo","RJ","28623030","2241667853","MelissaMarino@teleworm.us");
