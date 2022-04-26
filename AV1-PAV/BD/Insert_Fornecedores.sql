4209a7a882642bc51857419232ce349cf0903ed2
senhorbento <sbento.ti@gmail.com>
2022-04-26 20:10:30 -0300

    Adicionados mais dados aos scripts

--- AV1-PAV/BD/Insert_Fornecedores.sql
+++ AV1-PAV/BD/Insert_Fornecedores.sql
@@ -2,20 +2,49 @@ use pav_av1;
 
 -- Tabela fornecedor
 
-INSERT INTO fornecedor(id_fornecedor, nome, cpf_cnpj, logradouro, numero, complemento, bairro, cidade, estado, cep, telefone, email)
-VALUES (0, "Fornecedor não identificado", 00000000000000, "", 0, "", "", "", "", "", "", "");
+-- Template:
+-- INSERT INTO fornecedor(id_fornecedor, nome, cpf_cnpj, logradouro, numero, complemento, bairro, cidade, estado, cep, telefone, email)
+-- VALUES (id_fornecedor, "nome", "cpf_cnpj", "logradouro", numero, "complemento", "bairro", "cidade", "estado", "cep", "telefone", "email");
 
 INSERT INTO fornecedor(id_fornecedor, nome, cpf_cnpj, logradouro, numero, complemento, bairro, cidade, estado, cep, telefone, email)
-VALUES (1, "Camila e Henry Adega Ltda", 11006056000170, "Rua Nadir", 507, "", "Chácaras Arcampo", "Duque de Caxias", "RJ", "25251560", "2127872567", "comercial@camilaehenryadegaltda.com.br");
+VALUES (0, "Fornecedor nao identificado", "00000000000000", "", 0, "", "", "", "", "", "", "");
 
 INSERT INTO fornecedor(id_fornecedor, nome, cpf_cnpj, logradouro, numero, complemento, bairro, cidade, estado, cep, telefone, email)
-VALUES (2, "Bianca e Esther Distribuidora Ltda", 23551355000174, "Rua Interna Principal", 659, "", "Dois de Julho", "Camaçari", "BA", "42800600", "7128555663", "contato@biancaeestherdistribuidoraltda.com.br");
+VALUES (1, "Camila e Henry Adega Ltda", "11006056000170", "Rua Nadir", 507, "", "Chacaras Arcampo", "Duque de Caxias", "RJ", "25251560",
+ "2127872567", "comercial@camilaehenryadegaltda.com.br");
 
 INSERT INTO fornecedor(id_fornecedor, nome, cpf_cnpj, logradouro, numero, complemento, bairro, cidade, estado, cep, telefone, email)
-VALUES (3, "Vinicius e Emilly Alimentos Ltda", 95929829000180, "Travessa Vinte e Dois de Agosto", 306, "", "Pérola do Maicá", "Santarém", "PA", "68046090", "9328635514", "comercial@viniciuseemillyalimentosltda.com.br");
+VALUES (2, "Bianca e Esther Distribuidora Ltda", "23551355000174", "Rua Interna Principal", 659, "", "Dois de Julho", "Camaçari", "BA",
+ "42800600", "7128555663", "contato@biancaeestherdistribuidoraltda.com.br");
 
 INSERT INTO fornecedor(id_fornecedor, nome, cpf_cnpj, logradouro, numero, complemento, bairro, cidade, estado, cep, telefone, email)
-VALUES (4, "Alimentos Nova Era Ltda", 25232222046233, "Pernambuco", 306, "Aeroporto 6", "Vila Nova", "Boa Vista", "PA", "72391614", "95168254815", "contato@alimentos-nova-era.com.br");
+VALUES (3, "Vinicius e Emilly Alimentos Ltda", "95929829000180", "Travessa Vinte e Dois de Agosto", 306, "", "Perola do Maica", "Santarem",
+ "PA", "68046090", "9328635514", "comercial@viniciuseemillyalimentosltda.com.br");
 
 INSERT INTO fornecedor(id_fornecedor, nome, cpf_cnpj, logradouro, numero, complemento, bairro, cidade, estado, cep, telefone, email)
-VALUES (5, "Carolina e Emanuel Adega ME", 52776254000108, "Rua Betânia", 437, "", "Maracacuera (Icoaraci)", "Belém", "PA", "66815457", "9125502887", "compras@carolinaeemanueladegame.com.br");
+VALUES (4, "Alimentos Nova Era Ltda", "25232222046233", "Pernambuco", 306, "Aeroporto 6", "Vila Nova", "Boa Vista", "PA", "72391614",
+ "95168254815", "contato@alimentos-nova-era.com.br");
+
+INSERT INTO fornecedor(id_fornecedor, nome, cpf_cnpj, logradouro, numero, complemento, bairro, cidade, estado, cep, telefone, email)
+VALUES (5, "Carolina e Emanuel Adega ME", "52776254000108", "Rua Betania", 437, "", "Maracacuera (Icoaraci)", "Belem", "PA", "66815457",
+ "9125502887", "compras@carolinaeemanueladegame.com.br");
+
+INSERT INTO fornecedor(id_fornecedor, nome, cpf_cnpj, logradouro, numero, complemento, bairro, cidade, estado, cep, telefone, email)
+VALUES (6, "Açougue Bye Com","24446444000113", "Rua A", 1106, "Fazenda 6", " Alameda das Jabuticabeiras", "Vargem", "SP", "05936813", 
+"11993596732", "contato@acouguebye.com");
+
+INSERT INTO fornecedor(id_fornecedor, nome, cpf_cnpj, logradouro, numero, complemento, bairro, cidade, estado, cep, telefone, email)
+VALUES (7, "Francisca Santos Moraes", "11020651016", "Rua Belo Horizonte", 7662, "Anexo 8", "Sao Cristovao", "Sao Paulo", "SP", "10265447",
+ "11352648647", "contato@bancosantoagostinho.com");
+
+INSERT INTO fornecedor(id_fornecedor, nome, cpf_cnpj, logradouro, numero, complemento, bairro, cidade, estado, cep, telefone, email)
+VALUES (8, "Comércio Rainha", "35157656000105", "Rua Nei Palmeiro", 1978, "", "Barra da Tijuca", "Rio de Janeiro", "RJ", "22640240",
+ "2148236407", "contato@comérciorainha.com");
+
+INSERT INTO fornecedor(id_fornecedor, nome, cpf_cnpj, logradouro, numero, complemento, bairro, cidade, estado, cep, telefone, email)
+VALUES (9, "Peixaria Dois Irmaos", "30517686000153", "Rua Durvalino Peliciari", 813, "", "Distrito Industrial Antonio Della - Torre",
+ "Franca", "SP", "14406120", "1636463321", "doisirmaos@teuemail.com");
+
+INSERT INTO fornecedor(id_fornecedor, nome, cpf_cnpj, logradouro, numero, complemento, bairro, cidade, estado, cep, telefone, email)
+VALUES (10, "Abatedouro das Galinha", "21489214000143", "Travessa Rodolfo Conceição e Silva", 210, "", "Cooperativa", "São Bernardo do Campo",
+ "SP", "09851683", "16986106864", "abatedouro@seinaoemail.com.br");
--- AV1-PAV/BD/Insert_Produtos.sql
+++ AV1-PAV/BD/Insert_Produtos.sql
@@ -4,20 +4,70 @@ INSERT INTO produto(id_produto, nome, quantidade_estoque, preco, unidade, id_for
 VALUES (0,"Produto não identificado", 0, 0, "KG", 0);
 
 INSERT INTO produto(id_produto, nome, quantidade_estoque, preco, unidade, id_fornecedor)
-VALUES (1,"Batata", 10, 3.00, "KG", 0);
+VALUES (1,"Batata", 10, 3.00, "KG", 3);
 
 INSERT INTO produto(id_produto, nome, quantidade_estoque, preco, unidade, id_fornecedor)
-VALUES (2,"Batata doce", 10, 3.50, "KG", 0);
+VALUES (2,"Batata doce", 10, 3.50, "KG", 2);
 
 INSERT INTO produto(id_produto, nome, quantidade_estoque, preco, unidade, id_fornecedor)
-VALUES (3,"Vinho Tinto", 10, 30.00, "Un", 0);
+VALUES (3,"Vinho Tinto", 10, 30.00, "Un", 5);
 
 INSERT INTO produto(id_produto, nome, quantidade_estoque, preco, unidade, id_fornecedor)
-VALUES (4,"Vinho Branco", 10, 35.00, "Un", 0);
+VALUES (4,"Vinho Branco", 10, 35.00, "Un", 1);
 
 INSERT INTO produto(id_produto, nome, quantidade_estoque, preco, unidade, id_fornecedor)
-VALUES (5,"Barra de Chocolate Nestle", 10, 4.00, "Un", 0);
+VALUES (5,"Barra de Chocolate Nestle", 10, 4.00, "Un", 4);
 
 INSERT INTO produto(id_produto, nome, quantidade_estoque, preco, unidade, id_fornecedor)
 VALUES (6,"Barra de Chocolate Garoto", 10, 5.00, "Un", 0);
+
+INSERT INTO produto(id_produto, nome, quantidade_estoque, preco, unidade, id_fornecedor)
+VALUES (7,"Alcatra", 100, 50.00, "KG", 6);
+
+INSERT INTO produto(id_produto, nome, quantidade_estoque, preco, unidade, id_fornecedor)
+VALUES (8,"Bacon", 50, 20.00, "KG", 6);
+
+INSERT INTO produto(id_produto, nome, quantidade_estoque, preco, unidade, id_fornecedor)
+VALUES (9,"Suspiro", 10, 3.00, "Un", 7);
+
+INSERT INTO produto(id_produto, nome, quantidade_estoque, preco, unidade, id_fornecedor)
+VALUES (10,"Vodka Smirnoff", 50, 50.00, "UN", 10);
+
+INSERT INTO produto(id_produto, nome, quantidade_estoque, preco, unidade, id_fornecedor)
+VALUES (11,"Salmao", 20, 50.00, "UN", 9);
+
+INSERT INTO produto(id_produto, nome, quantidade_estoque, preco, unidade, id_fornecedor)
+VALUES (10,"Tilapia", 20, 30.00, "UN", 9);
+
+INSERT INTO produto(id_produto, nome, quantidade_estoque, preco, unidade, id_fornecedor)
+VALUES (11,"Sambiquira", 20, 5.00, "KG", 10);
+
+INSERT INTO produto(id_produto, nome, quantidade_estoque, preco, unidade, id_fornecedor)
+VALUES (12,"Asa de frango", 50, 20.00, "KG", 10);
+
+INSERT INTO produto(id_produto, nome, quantidade_estoque, preco, unidade, id_fornecedor)
+VALUES (13,"Coraçao de galinha", 50, 35.00, "KG", 10);
+
+INSERT INTO produto(id_produto, nome, quantidade_estoque, preco, unidade, id_fornecedor)
+VALUES (14,"Cafe pele", 30, 25.00, "UN", 8);
+
+INSERT INTO produto(id_produto, nome, quantidade_estoque, preco, unidade, id_fornecedor)
+VALUES (15,"Cafe pilao", 20, 20.00, "UN", 8);
+
+INSERT INTO produto(id_produto, nome, quantidade_estoque, preco, unidade, id_fornecedor)
+VALUES (16,"Cafe tres coraçoes", 10, 15.00, "UN", 8);
+
+INSERT INTO produto(id_produto, nome, quantidade_estoque, preco, unidade, id_fornecedor)
+VALUES (17,"Arroz Tio joao", 20, 30.00, "UN", 4);
+
+INSERT INTO produto(id_produto, nome, quantidade_estoque, preco, unidade, id_fornecedor)
+VALUES (18,"Arroz Camil", 60, 25.00, "UN", 4);
+
+INSERT INTO produto(id_produto, nome, quantidade_estoque, preco, unidade, id_fornecedor)
+VALUES (19,"Feijao Maximo", 50, 6.00, "UN", 4);
+
+INSERT INTO produto(id_produto, nome, quantidade_estoque, preco, unidade, id_fornecedor)
+VALUES (20,"Feijao Urbana", 50, 15.00, "UN", 4);
+
+
 
