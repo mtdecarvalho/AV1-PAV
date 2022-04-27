7bc5230f08b33e527c515bdfa163eaf822f0caef
senhorbento <sbento.ti@gmail.com>
2022-04-27 19:33:57 -0300

    Corrigido pequenos bugs na tela de venda

--- AV1-PAV/UI/AdicionarProduto.cs
+++ AV1-PAV/UI/AdicionarProduto.cs
@@ -61,7 +61,7 @@ namespace AV1_PAV.UI
         private void btnOK_Click(object sender, EventArgs e)
         {
             Produto produto = new();
-            produto.idProduto = ProdutoSQL.BuscarMaiorID() + 1;
+            produto.idProduto = ProdutoSQL.BuscarMaior("id_produto") + 1;
             produto.nome = tbxNome.Text;
             produto.qtdEstoque = int.Parse(tbxQtdEstoque.Text);
             produto.preco = double.Parse(tbxPreco.Text);
